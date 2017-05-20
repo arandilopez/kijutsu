e11609480ee2c7d660ac316d4f8c73881380be74
Arandi López <arandilopez.93@gmail.com>
2017-05-20 13:30:28 -0500

    Replace with arrow functions

--- test/AnimeServiceSpec.js
+++ test/AnimeServiceSpec.js
@@ -2,9 +2,9 @@ const chai = require('chai');
 const AnimeService = require('../lib/services/AnimeService');
 chai.should()
 
-describe('AnimeService', function () {
-  describe('#mapToTimes', function () {
-    it('should map anime entries to their times', function () {
+describe('AnimeService', () => {
+  describe('#mapToTimes', () => {
+    it('should map anime entries to their times', () => {
       let animes = [
         { progress: 10, anime: { episodeLength: 24 } },
         { progress: 10, anime: { episodeLength: 24 } },
--- test/TimeServiceSpec.js
+++ test/TimeServiceSpec.js
@@ -4,37 +4,37 @@ const chai = require('chai');
 const TimeService = require('../lib/services/TimeService');
 chai.should()
 
-describe('TimeService', function () {
-  describe('#parseMinsToHours', function () {
-    it('should parse 60 mins to 1 hour and 0 mins', function () {
+describe('TimeService', () => {
+  describe('#parseMinsToHours', () => {
+    it('should parse 60 mins to 1 hour and 0 mins', () => {
       let res = TimeService.parseMinsToHours(60) // mins
       res.hours.should.equal(1)
       res.mins.should.equal(0)
     })
 
-    it('should parse 80 mins to 1 hour and 20 mins', function () {
+    it('should parse 80 mins to 1 hour and 20 mins', () => {
       let res = TimeService.parseMinsToHours(80) //mins
       res.hours.should.equal(1)
       res.mins.should.equal(20)
     })
   })
 
-  describe('#parseHoursToDays', function () {
-    it('should parse 80 mins to 0 days 1 hours and 20 mins', function () {
+  describe('#parseHoursToDays', () => {
+    it('should parse 80 mins to 0 days 1 hours and 20 mins', () => {
       let res = TimeService.parseHoursToDays(80) //mins
       res.days.should.equal(0)
       res.hours.should.equal(1)
       res.mins.should.equal(20)
     })
 
-    it('should parse 1_440mins to 1days, 0hours and 0mins', function () {
+    it('should parse 1_440mins to 1days, 0hours and 0mins', () => {
       let res = TimeService.parseHoursToDays(1440) // mins
       res.days.should.equal(1)
       res.hours.should.equal(0)
       res.mins.should.equal(0)
     })
 
-    it('should parse 3_620min to 2days 12hours and 20mins', function () {
+    it('should parse 3_620min to 2days 12hours and 20mins', () => {
       let res = TimeService.parseHoursToDays(3620) // mins
       res.days.should.equal(2)
       res.hours.should.equal(12)
@@ -43,8 +43,8 @@ describe('TimeService', function () {
 
   })
 
-  describe('parseDaysToMonths', function () {
-    it('should parse 3_620min to 0months 2days 12hours and 20mins', function () {
+  describe('parseDaysToMonths', () => {
+    it('should parse 3_620min to 0months 2days 12hours and 20mins', () => {
       let res = TimeService.parseDaysToMonths(3620) // mins
       res.months.should.equal(0)
       res.days.should.equal(2)
@@ -52,7 +52,7 @@ describe('TimeService', function () {
       res.mins.should.equal(20)
     })
 
-    it('should parse 49_930min to 1months 4days 16hours and 10mins', function () {
+    it('should parse 49_930min to 1months 4days 16hours and 10mins', () => {
       let res = TimeService.parseDaysToMonths(49930) // mins
       res.months.should.equal(1)
       res.days.should.equal(4)
@@ -61,8 +61,8 @@ describe('TimeService', function () {
     })
   })
 
-  describe('splitTime', function () {
-    it('should parse 49_930min to 1months 4days 16hours and 10mins', function () {
+  describe('splitTime', () => {
+    it('should parse 49_930min to 1months 4days 16hours and 10mins', () => {
       let res = TimeService.splitTime(49930) // mins
       res.months.should.equal(1)
       res.days.should.equal(4)
@@ -71,8 +71,8 @@ describe('TimeService', function () {
     })
   })
 
-  describe('timeReducer', function () {
-    it('should reduce a coleccion of anime to a sum of their times', function () {
+  describe('timeReducer', () => {
+    it('should reduce a coleccion of anime to a sum of their times', () => {
       let times = [10, 20, 30, 40]
 
       let time = TimeService.timeReducer(times)
